b5503a8228def79e670d0993685db3f15c38c255
christganfon-art <christganfon@gmail.com>
2025-08-07 00:14:05 +0200

    Create src-main.tsx

---
+++ src-main.tsx
@@ -0,0 +1,10 @@
+importation Réagir de 'réagir'
+importation RéagirDOM de 'react-dom/client'
+importation App de './App.tsx'
+importation './index.css'
+
+RéagirDOM'.créerRoot(document'.getElementById(" racine ")!)'.rendre(
+  <Réagir'.Mode strict>
+    <App />
+  </Réagir'.Mode strict>,
+)
